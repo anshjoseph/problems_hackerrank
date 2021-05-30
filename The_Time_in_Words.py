d062455f66ac5ee6686192adbde7a9b0a875dd3d
R ansh joseph <ranshjosrph@gmail.com>
2021-05-30 08:47:22 +0530

    final code

--- The_Time_in_Words.py
+++ The_Time_in_Words.py
@@ -18,3 +18,40 @@ Input Format
 
 The first line contains , the hours portion The second line contains , the minutes portion
 """
+
+
+def timeInWords(H,M):
+    minutes = ['zero','one','two','three','four','five',
+           'six','seven','eight','nine','ten',
+           'eleven','twelve','thirteen','fourteen',
+           'fifteen','sixteen','seventeen','eighteen',
+           'nineteen','twenty','twenty one', 'twenty two',
+           'twenty three','twenty four','twenty five',
+           'twenty six','twenty seven','twenty eight',
+           'twenty nine', 'thirty']
+    hours = ['zero','one','two','three','four','five',
+         'six','seven','eight','nine','ten','eleven','twelve']
+    return_str = ""
+    if M == 0:
+        return_str += f"{hours[H]} o' clock"
+    elif M == 15:
+        return_str += f"quarter past {hours[H]}"
+    elif M == 30:
+        return_str +=f"half past {hours[H]}"
+    elif M == 45:
+        if H == 12:
+            return_str += f"quarter to {hours[1]}"
+        else:
+            return_str += f"quarter to {hours[H+1]}"
+    elif M > 0 and M < 30:
+        return_str += f"{minutes[M]} minutes past {hours[H]}"
+    elif M > 30 and M < 60:
+        if H == 12:
+            return_str += f"{minutes[60-M] } minutes to {hours[1]}"
+        else:
+            return_str += f"{minutes[60-M] } minutes to {hours[H + 1]}"
+    return return_str
+if __name__ == "__main__":
+    Hour = int(input())
+    Min = int(input())
+    print(timeInWords(Hour,Min)) 
